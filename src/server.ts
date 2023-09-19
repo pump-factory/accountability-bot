4c05064947a83973ee4861c85f1fe59d2041b9d5
Harrison Borges <hborges005@gmail.com>
2023-09-19 14:54:36 -0600

    init postgres client, add create command

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,42 @@
 import { Telegraf } from 'telegraf'
 import cron from 'node-cron'
+import { createHabit, findHabitByTitle } from './habits/habits.queries'
+import { Client } from 'pg'
+import { create } from 'domain'
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+})
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 bot.start((ctx) => ctx.reply("Welcome! Let's get accountable"))
 bot.help((ctx) => ctx.reply('Implement me!'))
 bot.on('text', (ctx) => {
 	ctx.reply(ctx.message)
+})
+
+bot.command('create', async (ctx) => {
+	const existing = await findHabitByTitle.run(
+		{
+			title: ctx.message.text,
+		},
+		client,
+	)
+
+	if (existing) {
+		ctx.reply('habit already exists')
+		return
+	}
+
+	await createHabit.run(
+		{
+			title: ctx.message.text,
+		},
+		client,
+	)
+
+	await ctx.reply('created new daily habit')
 })
 
 // scheduled messages
