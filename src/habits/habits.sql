b7d7e7bfe5295c8297ff187902f47485c8175690
Harrison Borges <hborges005@gmail.com>
2023-09-20 09:14:13 -0600

    reformat sql

--- src/habits/habits.sql
+++ src/habits/habits.sql
@@ -1,42 +1,27 @@
 /* @name FindHabits */
-select
-	*
-from
-	habits;
+select *
+from habits;
 
 /* @name FindHabitByTitle */
-select
-	*
-from
-	habits
-where
-	title = :title;
+select *
+from habits
+where title = :title;
 
 /* @name FindHabitCompletionsForUser */
-select
-	*
-from
-	habit_completions
-where
-	user_id = :user_id;
+select *
+from habit_completions
+where user_id = :user_id;
 
 /* @name FindHabitCompletionsForUserToday */
-select
-	*
-from
-	habit_completions
-where
-	user_id = :user_id
-	and completed_at > now() - interval '1 day';
+select *
+from habit_completions
+where user_id = :user_id
+  and completed_at > now() - interval '1 day';
 
 /* @name CreateHabit */
-insert into
-	habits (title, description)
-values
-	(:title, :description);
+insert into habits (title, description)
+values (:title, :description);
 
 /* @name LogHabitCompletion */
-insert into
-	habit_completions (user_id, habit_id, completed_at)
-values
-	(:user_id, :habit_id, now());
+insert into habit_completions (user_id, habit_id, completed_at)
+values (:user_id, :habit_id, now());
