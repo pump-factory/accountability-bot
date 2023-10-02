f82a663c91f6ae45a5e0b1bae643c03245f20115
Harrison Borges <hborges005@gmail.com>
2023-10-02 17:04:45 -0600

    reset all sequences

--- src/test/teardown.sql
+++ src/test/teardown.sql
@@ -2,3 +2,15 @@ drop table if exists users_chats;
 drop table if exists habit_completions;
 drop table if exists habits;
 drop table if exists users;
+
+DO
+$$
+    DECLARE
+        seqname text;
+    BEGIN
+        FOR seqname IN (SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema = 'public')
+            LOOP
+                EXECUTE 'ALTER SEQUENCE ' || seqname || ' RESTART WITH 1';
+            END LOOP;
+    END
+$$;
