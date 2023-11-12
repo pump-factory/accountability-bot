db937106c09683e627b1c1f4d527c5643e6cbe5d
Harrison Borges <hborges005@gmail.com>
2023-11-12 11:53:15 -0700

    use chatGPT to generate morning message

--- src/cron.ts
+++ src/cron.ts
@@ -2,7 +2,12 @@ import cron from 'node-cron'
 import { findHabitsGroupedByChatId } from './habits/habits.queries'
 import { client } from './db'
 import { bot } from './bot'
-import { findUsersWithoutHabitCompletions } from './users/users.queries'
+import {
+	findUsersInChat,
+	findUsersWithoutHabitCompletions,
+} from './users/users.queries'
+import { generateChatMessage } from './openai'
+import { ChatCompletionMessageParam } from 'openai/resources'
 
 export async function sendMorningReminder() {
 	const results = await findHabitsGroupedByChatId.run(undefined, client)
@@ -14,10 +19,23 @@ export async function sendMorningReminder() {
 		const habits = habitJson as { title: string; id: number }[]
 		const habitStr = habits.map((habit) => habit.title).join(', ')
 
-		await bot.telegram.sendMessage(
-			chatId,
-			"Good morning, accountability champions! 🌞 Today is a brand new opportunity to find your inner peace and clarity through meditation. Take a deep breath, commit to your practice, and let's make today another successful day on our journey to mindfulness and well-being. 🧘‍♀️🧘‍♂️ #MeditationMasters",
-		)
+		const chatUsers = await findUsersInChat.run({ chatId }, client)
+		const userNames = chatUsers.map((user) => user.name).join(', ')
+
+		const userMessage: ChatCompletionMessageParam = {
+			role: 'user',
+			content: `Good morning, ${userNames}! Today is a new opportunity to continue building positive habits. Whether it's ${habitStr} or any other personal goals, remember that each small step is a part of your journey towards success and well-being. Stay focused, stay motivated, and embrace the day with enthusiasm! You've got this!`,
+		}
+
+		let chatMessage: string
+		try {
+			chatMessage = await generateChatMessage([userMessage])
+		} catch (error) {
+			chatMessage = `Good morning, accountability champions! 🌞 Today is a brand new opportunity to find your inner peace and clarity through meditation. Take a deep breath, commit to your practice, and let's make today another successful day on our journey to mindfulness and well-being. 🧘‍♀️🧘‍♂️ #MeditationMasters`
+			console.error('Failed to generate chat message', error)
+		}
+
+		await bot.telegram.sendMessage(chatId, chatMessage)
 	}
 }
 
