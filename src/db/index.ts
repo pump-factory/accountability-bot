21f9b9e9cfdd34669f6032f166349f103f909932
Harrison Borges <hborges005@gmail.com>
2023-11-11 20:38:02 -0700

    take postgres options

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,8 @@
 import { Client } from 'pg'
 
-export const client: Client = new Client(process.env.DATABASE_URL)
+export const client: Client = new Client({
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DATABASE,
+	host: process.env.POSTGRES_HOST,
+})
